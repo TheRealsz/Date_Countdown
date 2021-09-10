defe1b1cdcb46c8d3c1965e6a5d6595e6091fbef
unknown <Robindiegoa@gmail.com>
2021-09-10 01:41:29 -0300

    Prevenção a input sem data, ajuste no design pos submit e botao desabilitado apos primeiro submit

--- JS/main_DC.js
+++ JS/main_DC.js
@@ -1,39 +1,70 @@
 function submit(){
-    const title = document.getElementById("inputTitle").value;
-    const data = new Date(document.getElementById("inputDate").value).getTime()
-    const titleUser = document.getElementsByClassName("titleUser")[0]
-    const dataUser = new Date(document.getElementById("inputDate").value).toLocaleString('pt-BR', {weekday: "long", day: "numeric", year: "numeric", month: "long", hour: "numeric", minute: "numeric", second: "numeric",})
+    if(document.getElementById('inputDate').value == ""){
+        alert("Coloque uma data")
+        return
+    } else{
+        const title = document.getElementById("inputTitle").value;
+        const data = new Date(document.getElementById("inputDate").value).getTime()
+        const titleUser = document.getElementsByClassName("titleUser")[0]
+        const dataUser = new Date(document.getElementById("inputDate").value).toLocaleString('pt-BR', {weekday: "long", day: "numeric", year: "numeric", month: "long", hour: "numeric", minute: "numeric", second: "numeric",})
+        const button = document.getElementsByClassName("btn btn-outline-dark")[0]
 
-    titleUser.innerHTML = title
-    titleUser.style.padding = "10px"
-    
-    document.getElementsByClassName("dateUser")[0].innerHTML = dataUser
-    
-    const func = setInterval(function() {
+        button.disabled = true
+
+        if (title !== "") {
+            titleUser.innerHTML = title
+            titleUser.style.padding = "10px"
+            
+            document.getElementsByClassName("dateUser")[0].innerHTML = dataUser
+            
+            const func = setInterval(function() {
+                
+                const dateNow = new Date().getTime()
+                const timeSubtr = data - dateNow
+                const days = Math.floor(timeSubtr / (1000 * 60 * 60 * 24))
+                const hours = Math.floor((timeSubtr % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const minutes = Math.floor((timeSubtr % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.floor((timeSubtr % (1000 * 60)) / 1000);
+                
         
-        const dateNow = new Date().getTime()
-        const timeSubtr = data - dateNow
-        const days = Math.floor(timeSubtr / (1000 * 60 * 60 * 24))
-        const hours = Math.floor((timeSubtr % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeSubtr % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeSubtr % (1000 * 60)) / 1000);
-        
-  
-        document.getElementsByClassName("countdown")[0].innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+                document.getElementsByClassName("countdown")[0].innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
 
 
-        if (timeSubtr < 0) {
-            clearInterval(func);
-            document.getElementsByClassName("countdown")[0].innerHTML = "EXPIRED";  
-          }
-  
+                if (timeSubtr < 0) {
+                    clearInterval(func);
+                    document.getElementsByClassName("countdown")[0].innerHTML = "EXPIRED";  
+                }
         
-        // document.getElementsByClassName("titleUser")[0].innerHTML = title;
-        // document.getElementsByClassName("dateUser")[0].innerHTML = data; //terminar mais pra frente
-        
-        // console.log(dateNow)
-        // console.log(timeNow)
-        
-    },1000)
 
+                
+            },1000)
+        } else {
+            document.getElementsByClassName("dateUser")[0].innerHTML = dataUser
+
+            const func = setInterval(function() {
+                
+                const dateNow = new Date().getTime()
+                const timeSubtr = data - dateNow
+                const days = Math.floor(timeSubtr / (1000 * 60 * 60 * 24))
+                const hours = Math.floor((timeSubtr % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const minutes = Math.floor((timeSubtr % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.floor((timeSubtr % (1000 * 60)) / 1000);
+                
+        
+                document.getElementsByClassName("countdown")[0].innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+
+
+                if (timeSubtr < 0) {
+                    clearInterval(func);
+                    document.getElementsByClassName("countdown")[0].innerHTML = "EXPIRED";  
+                }
+        
+            },1000)
+
+        }    
+        //Adicionar outro botao fazendo com que apague a data escolhida e deixe que a pessoa escolha outra data, habilitando o submit novamente
+    }        
 }
+
+
+//Ajustar para que o usuario coloque quantas datas ele quiser
